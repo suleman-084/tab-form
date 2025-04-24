054a8fee4fd025b6608acb2d170c5d418cfcabc8
Mohammed Suleman <111511704+suleman-084@users.noreply.github.com>
2025-04-24 21:04:29 +0000

    created the tabs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+// import TabForm from "./component/TabForm";
 import TabForm from "./component/TabForm";
 
 export default function App() {
+
+
   return (
     <div className="App">
-      <div>hello</div>
+      
       <TabForm />
     </div>
   );
--- src/component/TabForm.jsx
+++ src/component/TabForm.jsx
@@ -1,3 +1,30 @@
-export default Tabform = ()={
-    return <div>Tabform</div>
-}
+import Profile from "./Profile";
+import Settings from "./Settings";
+import Interests from "./interests";
+
+const TabForm = () => {
+    
+    const tabs = [{
+        name : "Profile",
+        component : Profile
+    },
+    {
+        name : "Interests",
+        component : Interests
+    },
+    {
+        name : "settings",
+        component : Settings
+    },
+    
+]
+
+    return <div>
+        <div>
+            {tabs.map((t)=><div>{t.name}</div>)}
+        </div>
+    </div>;
+  };
+  
+  export default TabForm;
+  
